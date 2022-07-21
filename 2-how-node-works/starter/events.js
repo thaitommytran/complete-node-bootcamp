e087185b68a97293b7957678db055fa3311a165f
Thai Tran <thai.tommy.tran@gmail.com>
2022-07-21 10:06:50 -0500

    section 4 completed

---
+++ 2-how-node-works/starter/events.js
@@ -0,0 +1,46 @@
+const EventEmmiter = require("events");
+const http = require("http");
+
+class Sales extends EventEmmiter {
+  constructor() {
+    super();
+  }
+}
+
+const myEmitter = new Sales();
+
+myEmitter.on("newSale", () => {
+  console.log("There was a new sale!");
+});
+
+myEmitter.on("newSale", () => {
+  console.log("Customer name: Thai");
+});
+
+myEmitter.on("newSale", (stock) => {
+  console.log(`There are now ${stock} items left in stock.`);
+});
+
+myEmitter.emit("newSale", 9);
+
+//////////////////
+
+const server = http.createServer();
+
+server.on("request", (req, res) => {
+  console.log("Request received!");
+  console.log(req.url);
+  res.end("Request received");
+});
+
+server.on("request", (req, res) => {
+  console.log("Another request 😃");
+});
+
+server.on("close", () => {
+  console.log("Server closed");
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Waiting for requests...");
+});
